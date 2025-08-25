83792b797643e743a80a09d9bfac986c728dd077
tareq shabaro <shabarotareq@gmail.com>
2025-08-25 16:38:40 +0300

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello Render 🚀</h1>
+      <p>React app is running successfully!</p>
+    </div>
+  );
+}
+
+export default App;
